073fd3754a9bd91c54634381ccc820bb080ec12f
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-03-09 22:29:52 -0300

    Add comments to problem 4 worksheet1

--- Worksheet 1/problem4.py
+++ Worksheet 1/problem4.py
@@ -4,12 +4,15 @@ def is_prime(n):
     """
     Returns True if n is prime, False otherwise.
     """
+    # Special cases for small numbers
     if n == -1 or n == 0 or n == 1:
         return False
     if n == -2 or n == 2:
         return True
+    # All other even numbers are not prime
     if n % 2 == 0:
         return False
+    # Check if n is a multiple of a number, besides 1 and itself
     aux = 3
     while aux <= math.ceil(math.sqrt(n)):
         if n % aux == 0:
