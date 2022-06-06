0bd8659cb76357b4e429a4bec5950981d5d690ef
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-06-05 21:31:33 -0300

    Add book and product register

--- Worksheet 9/register.py
+++ Worksheet 9/register.py
@@ -38,6 +38,12 @@ class Register(abc.ABC):
         """
         pass
 
+    def print_register(self):
+        """
+        Prints all elements in register.
+        """
+        print(self.register)
+
 
 class RegisterPerson(Register):
     """
@@ -61,6 +67,67 @@ class RegisterPerson(Register):
         None if it's not there.
         """
         for person in self.register:
-            if person.check_atribute("name") == element:
+            if person.check_atribute("name", element):
                 return person
         return None
+
+
+class RegisterProduct(Register):
+    """
+    Registration for products.
+    """
+    def __init__(self):
+        super().__init__()
+    
+    def modify_element(self, element, atribute, value):
+        """
+        Modifies an element's atribute to a value.
+        """
+        for product in self.register:
+            if product is element:
+                return product.modify_atribute(atribute, value)
+        return False
+
+
+class RegisterBook(RegisterProduct):
+    """
+    Registration for books.
+    """
+    def __init__(self, writer_register):
+        super().__init__()
+        self.writers = writer_register
+    
+    def add_element(self, element):
+        """
+        Adds new book to the register
+        """
+        for writer in self.writers:
+            if writer is element.get_atribute("writer"):
+                writer.modify_atribute("published_titles", element)
+        super().add_element(element)
+    
+    def del_element(self, element):
+        """
+        Deletes book from the register.
+        """
+        for i in range(len(element.writer.published_titles)):
+            if element.writer.published_titles[i] is element:
+                del element.writer.published_titles[i]
+                return super().del_element(element)
+        return False
+
+    def find_element(self, element):
+        """
+        Finds the book by their title in the registration.
+        None if it's not there.
+        """
+        for book in self.register:
+            if book.check_atribute("title", element):
+                return book
+        return None
+
+    def find_books_writer(self, writer_name):
+        """
+        Returns all published titles by a writer.
+        """
+        return self.writers.find_element(writer_name)
